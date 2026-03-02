b46cd7feaa0397d0d9a4718ffcf94c3f576a79b3
Aaron <ajbasa@up.edu.ph>
2026-03-02 10:47:11 +0800

    fix(suplay): serve media files regardless of DEBUG setting (broken product images on prod)

--- suplay_app/office_supplies_project/urls.py
+++ suplay_app/office_supplies_project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +13,9 @@ urlpatterns = [
     path('sso/', include('allauth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files (product images, order docs, etc.)
+# Note: static() only works with DEBUG=True; re_path+serve works always.
+# All environments use runserver (not gunicorn), so this is safe.
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
